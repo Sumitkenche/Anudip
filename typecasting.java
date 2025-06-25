4e44714548e8f57ccf683ad894b14d37a1fd92fa
Sumitkenche <sumitkenche116@gmail.com>
2025-06-25 16:16:57 +0530

    Add files via upload

---
+++ typecasting.java
@@ -0,0 +1,18 @@
+package Anudip;
+public class typecasting
+{
+
+    public static void main(String args[])
+    {
+                    int a=20;     
+                    double b=a;
+                    System.out.println("b= "+b);
+
+         double c=99.99;
+         int d=(int)c;
+         System.out.println("d= "+d); //explicite larger double(8)to smaller int(4)
+
+
+         
+    }
+}
