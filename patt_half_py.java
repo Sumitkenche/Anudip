40407bb152c8896235e057ef4b87ae9e6370acf7
Sumitkenche <sumitkenche116@gmail.com>
2025-06-25 16:02:26 +0530

    Add files via upload

---
+++ patt_half_py.java
@@ -0,0 +1,80 @@
+package Anudip;
+public class patt_half_py
+{
+    public static void main (String args[])
+    {
+        int a=5;
+        for(int i=1;i<=a;i++)
+        {
+            for (int j=1;j<=i;j++)
+            {
+                System.out.print(" * ");
+            }
+                                 System.out.println();
+
+        }
+    }
+
+/*
+    *
+    * *
+    * * *
+    * * * *
+    * * * * *
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void main(String args[])
+    {
+        int c=5;
+       
+        for(int i=1;i<=c; i++)
+        {
+            for(int j=1; j<=i; j++)
+            {
+                System.out.print("*");
+            }
+           System.out.println();
+        }
+    }
+}
