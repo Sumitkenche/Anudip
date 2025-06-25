6da3522f9ca50e7c6494dd4fb0811cb46da9b48f
Sumitkenche <sumitkenche116@gmail.com>
2025-06-25 15:56:45 +0530

    Add files via upload

---
+++ one.java
@@ -0,0 +1,26 @@
+package Anudip;
+import java.util.Scanner;
+public class one {
+   
+    public static int add(int a, int b)
+    {
+        
+        int res=a+b;
+        return res;
+    }
+        public static void main(String args[])
+    {
+       Scanner sc=new Scanner(System.in);
+       System.out.println("Enter any two number");
+      
+       int a=sc.nextInt();
+       int b=sc.nextInt();
+
+       System.out.println("sum is "+add(a,b));
+    }
+    
+}
+//instance variable->heap
+//local variable ->stack
+//garbage collection
+//functions and constructor
