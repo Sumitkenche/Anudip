0d41f5195970ec811c0562b7bf558951c24dc3e1
Sumitkenche <sumitkenche116@gmail.com>
2025-06-26 15:59:58 +0530

    Add files via upload

---
+++ do_while.java
@@ -0,0 +1,18 @@
+package Anudip;
+
+public class do_while
+{
+    public static void main(String args[])
+    {
+        int i=0;
+        do
+        {
+            System.out.println("sumit");
+            i++;
+        }while(i<0);
+    }
+}
+/*25/06
+do while,forloop
+while
+functions creation ,parametarized.*/
