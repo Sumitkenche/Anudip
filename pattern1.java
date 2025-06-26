c6cdc555e6dc8e6aa15369ec2be039d8ca48a04f
Sumitkenche <sumitkenche116@gmail.com>
2025-06-26 11:49:15 +0530

    Add files via upload

---
+++ pattern1.java
@@ -0,0 +1,27 @@
+package Anudip;
+public class  pattern1
+{
+    public static void main(String args[])
+    {
+        int n=5;
+        for(int i=n; i>=1;i--)//n to 1
+        {
+            for(int j=1; j<=i;j++)//n stars to 1 star as per i value
+            {
+                System.out.print("*");
+            }   
+            System.out.println(" ");     
+    }
+        }
+        
+}
+
+/*
+ *****
+ ****
+ ***
+ **
+ *
+
+
+ */
